456d5153a3522e1780e873583eb2924ecb0fdb8b
kgeorgeblake <gxb231@cwru.edu>
2020-10-14 22:10:58 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 
 const dbConfig = process.env.MONGODB_URI;
 
+
+
 async function connection (){
-    await mongoose.connect (dbConfig, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, () =>
-    console.log ("Connected to DB")
-    );
+    await mongoose.connect(
+        process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false
+        }
+      );
+    
     }
 
     module.exports = connection;
